442e8534cf7c967c6edd7691468989105dec97fe
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2026-09-07 17:48:41 +0900

    Insert white space before block

--- bindings/ruby/ext/options.rb
+++ bindings/ruby/ext/options.rb
@@ -105,7 +105,7 @@ class Options
     FileUtils.mkpath File.dirname(cache_path)
     File.open cache_path, "w" do |file|
       @options.reject {|name, (type, value)| value.nil?}.each do |name, (type, value)|
-        line = 'set(%<name>s %<value>s CACHE %<type>s "" FORCE)' %{
+        line = 'set(%<name>s %<value>s CACHE %<type>s "" FORCE)' % {
           name:,
           type:,
           value: value == true ? "ON" : value == false ? "OFF" : escape_cmake(value)
